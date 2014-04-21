d054a0e4624e1c5b84b1b28625c92371dc077229
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2014-04-21 10:02:48 -0400

    An a bit more modular approach

--- data/xml-parse.sh
+++ data/xml-parse.sh
@@ -3,13 +3,39 @@
 # Dont really parse it, just do some nassty tricks to make a subset of
 # the xml that makes sense
 
+#!/bin/sh
+
 ORIGINAL_XML=/scratch/cperivol/wikipedia-mirror/drafts/wikipedia-parts/enwiki-20131202-pages-articles20.xml-p011125004p013324998.fix.xml
+
+function file_range {
+    file=$1
+    start=$2
+    len=$3
+
+    case $len in
+	"end") dd ibs=1 if=$file skip=$start; return 0;;
+	"start") dd ibs=1 if=$file count=$start; return 0;;
+	"") echo "You need to tell me <filename> <byte start> <length|'start'|'end'>" 1>&2; return 1;;
+    esac
+
+    if [[ $len -gt 0 ]]; then
+	# Dump to stdout
+	dd ibs=1 if=$file skip=$start count=$len;
+    else
+	if [[ $(($start - $len)) -lt 0 ]]; then
+	    len=$start
+	else
+	    len=$(($len -  2 * $len)) # -len
+	fi
+
+	# Dump to stdout
+	dd ibs=1 if=$file skip=$(($start - $len)) count=$len
+    fi
+}
 
 # Throw page in stdout
 function xml_page {
-
     term="<title>$@</title>"
-  
     title_offset=$(grep -b -F "$term" -m 1 $ORIGINAL_XML | grep -o "[0-9]*" | head -1)
 
     if [ ! $title_offset ]; then
@@ -23,16 +49,14 @@ function xml_page {
 	count=$title_offset
     fi
 
-#    echo PROCESSING COUNT: $count
+    # echo PROCESSING COUNT: $count 1>&2
     dd if=$ORIGINAL_XML count=$count skip=$(($title_offset-$count)) ibs=1 | tac | sed '/<page>/q'
     dd if=$ORIGINAL_XML skip=$title_offset ibs=1 | sed -n '/<\/page>/{p;q};p'
 }
 
 # Throw everything but the page in stdout
 function neg_xml_page {
-
     term="<title>$@</title>"
-  
     title_offset=$(grep -b -F "$term" -m 1 $ORIGINAL_XML | grep -o "[0-9]*" | head -1)
 
     if [ ! $title_offset ]; then
@@ -41,18 +65,15 @@ function neg_xml_page {
 	exit 0
     fi
 
-    count=1000
-    if [[ $title_offset -lt 1000 ]]; then
-	count=$title_offset
-    fi
+    to_page_start=$(file_range $ORIGINAL_XML $title_offset -1000 | tac | grep -b  -F "<page>" -m 1 | grep -o "[0-9]*")
+    to_page_end=$(($(file_range $ORIGINAL_XML $title_offset end | grep -b -F "</page>" -m 1 | grep -o "[0-9]*")+7))
+    page_start=$(($title_offset + $to_page_start))
+    page_end=$(($title_offset + $to_page_end))
 
-#    echo PROCESSING COUNT: $count
-    bytes_from_title=$(dd if=$ORIGINAL_XML count=$count skip=$(($title_offset-$count)) ibs=1 | tac | grep -b  -F "<page>" -m 1 | grep -o "[0-9]*")
-    page_end=$(dd if=$ORIGINAL_XML skip=$title_offset ibs=1 | grep -b -F "</page>" -m 1 | grep -o "[0-9]*")+7
-    page_start=$(($title_offset-$bytes_from_title))
+    echo -e "\tpage start: $page_start\n\tpage end: $page_end,\n\tbytes to copy: $(($(du -b $ORIGINAL_XML | awk '{print $1}') - $page_start + $page_end))" 1>&2
 
-    dd if=$ORIGINAL_XML count=$page_start ibs=1
-    dd if=$ORIGINAL_XML skip=$page_end ibs=1
+    file_range $ORIGINAL_XML $page_start start
+    file_range $ORIGINAL_XML $page_end end
 }
 
 # Put stdin betwinn mediawiki tags and into stdout
@@ -60,5 +81,5 @@ function mediawiki_xml {
     (head -1 $ORIGINAL_XML; sed -n "/<siteinfo>/,/<\/siteinfo>/p;/<\/siteinfo>/q" $ORIGINAL_XML ; cat - ; tail -1 $ORIGINAL_XML )
 }
 
-# Create a file WITHOUT the pages in args (remove neg for JUST those pages)
-(for i; do neg_xml_page "$i"; done) | mediawiki_xml
+# (for i; do xml_page "$i"; done) | mediawiki_xml
+neg_xml_page $1
